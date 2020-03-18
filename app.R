2f0fd0e5ce2870f5b7da345e64d5914038a83128
Claudio Zanettini <claudio.zanettini@gmail.com>
2020-03-17 22:23:23 -0400

    added log scale switch

--- app.R
+++ app.R
@@ -3,156 +3,152 @@
 source("other_f.R")
 
 ui <- dashboardPage(
-    dashboardHeader(title = "COVID-19",
-                    tags$li(class = "dropdown", actionButton("license", 
-                                                             label = "about",
-                                                             style = "text-transform: lowercase; font-style: italic;")
-                            )
-                    ),
-    dashboardSidebar(
-      disable = TRUE
-    ),  
-    dashboardBody(
-      shinyDashboardThemes(
-        theme = "boe_website"
-      ),
-      fluidRow(
-        box(
-        textOutput("updated")
-        )
-      ),
-      fluidRow(
-        box(
-          h2("Total Number of COVID-19 Cases"),
-          selectizeInput(
-            inputId = "imp_state",
-            label = "Select one or more States (max 4):",
-            choices = as.character(c("US", states)),
-            selected = c("Maryland"),
-            multiple = TRUE,
-            options = list(maxItems = 4)),
-          
-            uiOutput("body_UI")
-            # plotlyOutput("cases", height = "400px")
+  dashboardHeader(
+    title = "COVID-19",
+    tags$li(class = "dropdown", actionButton("license",
+      label = "about",
+      style = "text-transform: lowercase; font-style: italic;"
+    ))
+  ),
+  dashboardSidebar(
+    disable = TRUE
+  ),
+  dashboardBody(
+    shinyDashboardThemes(
+      theme = "boe_website"
+    ),
+    fluidRow(
+      box(
+        div(
+          style = "display: inline-block;vertical-align: center;",
+          textOutput("updated")
         ),
-        
-        box(
-          h2("Total Number of Specimens Tested for COVID-19 in U.S."),
-          h4( "Data of Confirmed, Deaths and Recovered from", 
-          a(href="https://github.com/CSSEGISandData/COVID-19", "JHU."),
-          br(),
-          "Data of Specimens Tested from",
-          a(href="https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/testing-in-us.html", "CDC."),
-          hr(style =  " margin-top: 1.1em; margin-bottom: 1.2em;")
-          ),
-              
-          
-          plotlyOutput("tested", height = "400px")
-          
+        div(
+          style = "display: inline-block; vertical-align: center; float:right",
+          materialSwitch("log_scale", "Y-axis in log2", value = FALSE)
         )
       )
-        
+    ),
+    fluidRow(
+      box(
+        h2("Total Number of COVID-19 Cases"),
+        selectizeInput(
+          inputId = "imp_state",
+          label = "Select one or more States (max 4):",
+          choices = as.character(c("US", states)),
+          selected = c("Maryland"),
+          multiple = TRUE,
+          options = list(maxItems = 4)
+        ),
+
+        uiOutput("body_UI")
+      ),
+
+      box(
+        h2("Total Number of Specimens Tested for COVID-19 in U.S."),
+        h4(
+          "Data of Confirmed, Deaths and Recovered from",
+          a(href = "https://github.com/CSSEGISandData/COVID-19", "JHU."),
+          br(),
+          "Data of Specimens Tested from",
+          a(href = "https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/testing-in-us.html", "CDC."),
+          hr(style = " margin-top: 1.1em; margin-bottom: 1.2em;")
+        ),
+
+
+        plotlyOutput("tested", height = "400px")
+      )
     )
-        
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-  
+
   # # Get data -----
-  
+
   withProgress(
     message = "retrieving the data...",
     {
-      dat_US <-  get_cases()
+      dat_US <- get_cases()
       tests <- get_tests()
-    })
+    }
+  )
 
-  
+
   dat_f <- reactiveVal(NULL)
- 
+
   dat_f <- reactive({
-     x <- input$imp_state
-     dat_US %>%
-       filter(state %in% x)
-   })
+    x <- input$imp_state
+    dat_US %>%
+      filter(state %in% x)
+  })
 
-  
 
-  
-
-  
-  
-    # license --------
-    observeEvent(
-      ignoreNULL = TRUE,
-      eventExpr = {
-        input$license
-      },
-      handlerExpr = {
-        sendSweetAlert(
-          session = session,
-          title = NULL,
-          html = TRUE,
-          text =
-            HTML("
+  # license --------
+  observeEvent(
+    ignoreNULL = TRUE,
+    eventExpr = {
+      input$license
+    },
+    handlerExpr = {
+      sendSweetAlert(
+        session = session,
+        title = NULL,
+        html = TRUE,
+        text =
+          HTML("
              2020 Claudio Zanettini, <br>
              <a href=\"https://github.com/c1au6i0/COVID-19\"> Code of the app </a>"),
-          width = "400px"
-        )
-      }
-    )
-    
+        width = "400px"
+      )
+    }
+  )
+
   # Last update -----
   output$updated <- renderText({
     m_date <- max(dat_f()$date)
-    
+
     x <- paste0("Last Update: ", m_date)
     x
-    
   })
-  
-  
-    # render test plot -----
-    output$tested <- renderPlotly({
-        p <- plot_tested(tests)
-    })
-    
-    
-    # show_p <- reactiveVal(value = FALSE)
-    # output$show_p <- reactive({show_p()})
-    # outputOptions(output, "show_p", suspendWhenHidden = FALSE)
 
-    
-    # Render plot_ cases -----
-    output$cases <- renderPlotly({
-        plot_cases(dat = dat_f())
-      })
-  
-  output$body_UI <- renderUI ({
+
+  # render test plot -----
+  output$tested <- renderPlotly({
+    p <- plot_tested(tests, log_scale = input$log_scale)
+  })
+
+
+  # show_p <- reactiveVal(value = FALSE)
+  # output$show_p <- reactive({show_p()})
+  # outputOptions(output, "show_p", suspendWhenHidden = FALSE)
+
+
+  # Render plot_ cases -----
+  output$cases <- renderPlotly({
+    plot_cases(dat = dat_f(), log_scale = input$log_scale)
+  })
+
+  output$body_UI <- renderUI({
     n_cond <- length(unique(dat_f()$condition))
-    
-    if(n_cond <= 1) {
+
+    if (n_cond <= 1) {
       p <- plotlyOutput("cases", height = "400px")
     }
 
-  if(n_cond == 2){
-    p <- plotlyOutput("cases", height = "800px")
-  }
+    if (n_cond == 2) {
+      p <- plotlyOutput("cases", height = "800px")
+    }
 
-  if(n_cond == 3){
-    p <- plotlyOutput("cases", height = "1200px")
-  }
+    if (n_cond == 3) {
+      p <- plotlyOutput("cases", height = "1200px")
+    }
     p
   })
-  
-    
-
-  
-    
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
 
 # https://stackoverflow.com/questions/38826893/shinydashboard-does-not-work-with-uioutput
--- other_f.R
+++ other_f.R
@@ -21,108 +21,115 @@ library(vroom)
 
 
 # Get data ------
-states <- c("Washington", "New York", "California", "Massachusetts", "Diamond Princess", 
-            "Grand Princess", "Georgia", "Colorado", "Florida", "New Jersey", 
-            "Oregon", "Texas", "Illinois", "Pennsylvania", "Iowa", "Maryland", 
-            "North Carolina", "South Carolina", "Tennessee", "Virginia", 
-            "Arizona", "Indiana", "Kentucky", "District of Columbia", "Nevada", 
-            "New Hampshire", "Minnesota", "Nebraska", "Ohio", "Rhode Island", 
-            "Wisconsin", "Connecticut", "Hawaii", "Oklahoma", "Utah", "Kansas", 
-            "Louisiana", "Missouri", "Vermont", "Alaska", "Arkansas", "Delaware", 
-            "Idaho", "Maine", "Michigan", "Mississippi", "Montana", "New Mexico", 
-            "North Dakota", "South Dakota", "West Virginia", "Wyoming", "Alabama", 
-            "Puerto Rico", "Virgin Islands, U.S.", "Guam")
+states <- c(
+  "Washington", "New York", "California", "Massachusetts", "Diamond Princess",
+  "Grand Princess", "Georgia", "Colorado", "Florida", "New Jersey",
+  "Oregon", "Texas", "Illinois", "Pennsylvania", "Iowa", "Maryland",
+  "North Carolina", "South Carolina", "Tennessee", "Virginia",
+  "Arizona", "Indiana", "Kentucky", "District of Columbia", "Nevada",
+  "New Hampshire", "Minnesota", "Nebraska", "Ohio", "Rhode Island",
+  "Wisconsin", "Connecticut", "Hawaii", "Oklahoma", "Utah", "Kansas",
+  "Louisiana", "Missouri", "Vermont", "Alaska", "Arkansas", "Delaware",
+  "Idaho", "Maine", "Michigan", "Mississippi", "Montana", "New Mexico",
+  "North Dakota", "South Dakota", "West Virginia", "Wyoming", "Alabama",
+  "Puerto Rico", "Virgin Islands, U.S.", "Guam"
+)
 
-get_cases <- function(){
-  
+get_cases <- function() {
   COVID <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-"
-  dat <- map_dfr(list("Confirmed", "Deaths", "Recovered"), function(x){
-    
-    url_dat <- paste(COVID, x, ".csv", sep ="")
+  dat <- map_dfr(list("Confirmed", "Deaths", "Recovered"), function(x) {
+    url_dat <- paste(COVID, x, ".csv", sep = "")
     dat <- vroom(file = url_dat)
     dat <- add_column(dat, condition = x, .before = 1)
     dat
   })
-  
+
   # Clean data -----
-  dat_US <-  dat %>%
+  dat_US <- dat %>%
     rename(state = `Province/State`, country = `Country/Region`) %>%
-    filter(country %in% "US" ) %>%
-    pivot_longer(6: ncol(dat), names_to = "date", values_to = "cases") %>%
-    mutate(date  = lubridate::mdy(date))
-  
+    filter(country %in% "US") %>%
+    pivot_longer(6:ncol(dat), names_to = "date", values_to = "cases") %>%
+    mutate(date = lubridate::mdy(date))
+
   dat_US[dat_US == 0] <- NA
-  
-  dat_US <-   na.omit(dat_US)
-  
+
+  dat_US <- na.omit(dat_US)
+
   # tot by day in US
   all_US <- dat_US %>%
     group_by(condition, date) %>%
     summarize(cases = sum(cases, na.rm = TRUE)) %>%
     mutate(state = "US", country = "US", Lat = 37.0902, Long = 95.7129)
-  
+
   all_US <- all_US[, names(dat_US)]
-  
+
   dat_US <- bind_rows(dat_US, all_US)
-  
+
   dat_US
 }
 
-get_tests <- function(){
+get_tests <- function() {
   # Get number of tests -----
   cdc_page <- xml2::read_html("https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/testing-in-us.html")
   tab_cdc <- cdc_page %>%
     rvest::html_nodes("table") %>%
-    rvest::html_table() 
-  
-  tests <-  tab_cdc [[1]] %>% 
-    data.frame() %>% 
-    clean_names() %>% 
-    mutate(date_collected = lubridate::mdy(paste0(date_collected, "/2020"))) %>% 
-    pivot_longer(2:3, names_to = "lab_type", values_to = "n") %>% 
-    mutate(n = as.numeric(n)) %>% 
-    na.omit() 
+    rvest::html_table()
+
+  tests <- tab_cdc [[1]] %>%
+    data.frame() %>%
+    clean_names() %>%
+    mutate(date_collected = lubridate::mdy(paste0(date_collected, "/2020"))) %>%
+    pivot_longer(2:3, names_to = "lab_type", values_to = "n") %>%
+    mutate(n = as.numeric(n)) %>%
+    na.omit()
   tests
 }
 
 
 # Plot tests -----
 
-plot_tested <- function(dat = tests) {
-  tests <- dat %>% 
-    group_by(date_collected) %>% 
+plot_tested <- function(dat = tests, log_scale = FALSE) {
+  tests <- dat %>%
+    group_by(date_collected) %>%
     summarize(d_test = sum(n))
-  
+
   tests[, "tot"] <- cumsum(tests$d_test)
-  
-  tests <- tests %>% 
-    separate(date_collected, sep = -5, into = c("y", "m_d")) %>% 
+
+  tests <- tests %>%
+    separate(date_collected, sep = -5, into = c("y", "m_d")) %>%
     mutate(group = 1)
-  
+
   n_date <- length(unique(tests$m_d))
-  
-  breaks <- unique(tests$m_d)[seq(1, n_date, ceiling(n_date/5))]
-  
-  p <- tests %>% 
+
+  breaks <- unique(tests$m_d)[seq(1, n_date, ceiling(n_date / 5))]
+
+  p <- tests %>%
     rename(date = m_d) %>%
     ggplot(aes(x = date, y = tot, group = group), color = "black") +
     geom_line(show.legend = FALSE, size = 0.2) +
     geom_point(size = 2, stroke = 0.2, fill = "#999999", shape = 21) +
-    labs(x ="", y = "") +
+    labs(x = "", y = "") +
     scale_x_discrete(breaks = breaks) +
-    scale_y_continuous(limits = c(0, ceiling(max(tests$tot)/5000) *5000)) +
     theme_bw() +
-    theme(legend.title = element_blank(),
-          legend.position = "top",
-          axis.text.x = element_text( hjust = 1)) 
-  p <- ggplotly(p, tooltip = c("y", "x", "shape")) %>% 
+    theme(
+      legend.title = element_blank(),
+      legend.position = "top",
+      axis.text.x = element_text(hjust = 1)
+    )
+  if (log_scale == FALSE) {
+    p <- p + scale_y_continuous(limits = c(0, ceiling(max(tests$tot) / 5000) * 5000))
+  } else {
+    p <- p + scale_y_continuous(trans = "log2", limits = function(x) c(2, ceiling(sqrt(max(x)))^2))
+  }
+
+  p <- ggplotly(p, tooltip = c("y", "x", "shape")) %>%
     layout(
       xaxis = list(fixedrange = TRUE),
       yaxis = list(fixedrange = TRUE)
     )
-  
+
   p <- p %>%
-    config(p = ., displayModeBar =FALSE)
+    config(p = ., displayModeBar = FALSE)
   p
 }
 
@@ -134,43 +141,48 @@ plot_tested <- function(dat = tests) {
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 
-plot_cases <- function(dat){
-
-  if(nrow(dat) > 0){
+plot_cases <- function(dat, log_scale = FALSE) {
+  if (nrow(dat) > 0) {
     # shapes of symbols
     shapes_l <- 21:24
     shapes_plot <- shapes_l[1:length(unique(dat$state))]
-    
-    dat_f <- dat %>% 
-      arrange(date) %>% 
+
+    dat_f <- dat %>%
+      arrange(date) %>%
       separate(date, sep = -5, into = c("y", "m_d"))
-    
+
     n_date <- length(unique(dat_f$m_d))
-    
+
     val <- unique(dat_f$m_d)
-    
-    breaks <- unique(dat_f$m_d)[seq(1, n_date, ceiling(n_date/5))]
-    
-    p <- 
-      dat_f %>% 
-      rename(date = m_d) %>% 
-      ggplot(aes(x = date, y = cases, fill = state, shape = state, group = state),color = "black") +
+
+    breaks <- unique(dat_f$m_d)[seq(1, n_date, ceiling(n_date / 5))]
+
+    p <- dat_f %>%
+      rename(date = m_d) %>%
+      ggplot(aes(x = date, y = cases, fill = state, shape = state, group = state), color = "black") +
       geom_point(size = 2, stroke = 0.2) +
       geom_line(show.legend = FALSE, size = 0.2) +
       scale_shape_manual(values = shapes_plot) +
       scale_fill_manual(values = cbPalette) +
-      scale_alpha(guide = 'none') +
-      labs(x ="", y = "") +
-      facet_wrap(vars(condition), ncol =  1, scales = "free") +
+      scale_alpha(guide = "none") +
+      labs(x = "", y = "") +
+      facet_wrap(vars(condition), ncol = 1, scales = "free") +
       scale_x_discrete(breaks = breaks, limits = val) +
-      scale_y_continuous(limits = c(0, NA),breaks = function(x) unique(floor(pretty(seq(0, (max(x) + 1) * 1.1))))) +
       theme_bw() +
-      theme(legend.title = element_blank(),
-            legend.position = "top",
-            strip.background = element_rect(fill="grey90"),
-            axis.text.x = element_text( hjust = 1))
-    
-    p <- ggplotly(p, tooltip = c("y", "x", "shape")) %>%
+      theme(
+        legend.title = element_blank(),
+        legend.position = "top",
+        strip.background = element_rect(fill = "grey90"),
+        axis.text.x = element_text(hjust = 1)
+      )
+
+    if (log_scale == FALSE) {
+      p_scale <- p + scale_y_continuous(limits = c(0, NA), breaks = function(x) unique(floor(pretty(seq(0, (max(x) + 1) * 1.1)))))
+    } else {
+      p_scale <- p + scale_y_continuous(trans = "log2", limits = function(x) c(2, ceiling(sqrt(max(x)))^2))
+    }
+
+    p_plotly <- ggplotly(p_scale, tooltip = c("y", "x", "shape")) %>%
       layout(
         legend = list(orientation = "h"),
         xaxis = list(fixedrange = TRUE),
@@ -179,21 +191,22 @@ plot_cases <- function(dat){
         yaxis2 = list(fixedrange = TRUE),
         xaxis3 = list(fixedrange = TRUE),
         yaxis3 = list(fixedrange = TRUE)
-      ) %>% 
-      config(p = ., displayModeBar =FALSE)
+      ) %>%
+      config(displayModeBar = FALSE)
   }
-  
-  if(nrow(dat)== 0){
+
+  if (nrow(dat) == 0) {
     df <- data.frame()
 
-    p <- ggplot(df) + geom_point() +
-    theme_void() +
-    annotate("text", x =1 , y =1, label = "No Case Reported")
+    p <- ggplot(df) +
+      geom_point() +
+      theme_void() +
+      annotate("text", x = 1, y = 1, label = "No Case Reported")
 
-    p <- ggplotly(p) %>%
-      config(displayModeBar =FALSE)
+    p_plotly <- ggplotly(p) %>%
+      config(displayModeBar = FALSE)
   }
-  
-  p
-  
+
+  p_plotly
 }
+
