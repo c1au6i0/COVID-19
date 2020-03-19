e992cc81d7cfa520859ad0ceaf6b598e34e5265a
Claudio Zanettini <claudio.zanettini@gmail.com>
2020-03-19 02:37:17 -0400

    fixed latency

--- app.R
+++ app.R
@@ -6,8 +6,8 @@ ui <- dashboardPage(
   dashboardHeader(
     title = "COVID-19",
     tags$li(class = "dropdown", actionButton("license",
-      label = "about",
-      style = "text-transform: lowercase; font-style: italic;"
+                                             label = "about",
+                                             style = "text-transform: lowercase; font-style: italic;"
     ))
   ),
   dashboardSidebar(
@@ -17,82 +17,98 @@ ui <- dashboardPage(
     shinyDashboardThemes(
       theme = "boe_website"
     ),
-        fluidRow(
-          box(
-            div(
-              style = "display: inline-block;vertical-align: center;",
-              textOutput("updated")
-            ),
-            div(
-              style = "display: inline-block; vertical-align: center; float:right",
-              materialSwitch("log_scale", "Y-axis in log2", value = FALSE)
-            )
-          )
+    fluidPage(
+    fluidRow(
+      box(
+        div(
+          style = "display: inline-block;vertical-align: center;",
+          textOutput("updated")
         ),
-        fluidPage(
-          box(
-            h2("Total Number of COVID-19 Cases"),
-            selectizeInput(
-              inputId = "imp_state",
-              label = "Select one or more States (max 4):",
-              choices = as.character(c("US", states)),
-              selected = c("Maryland"),
-              multiple = TRUE,
-              options = list(maxItems = 4)
-            ),
-            
-            uiOutput("body_UI")
-            ),
+        div(
+          style = "display: inline-block; vertical-align: center; float:right",
+          materialSwitch("log_scale", "Y-axis in log2", value = FALSE)
+        ))),
+    fluidRow(
+      box(
+        div(style = "display: inline-block; vertical-align: center; float:left",
+        h2("Total Number of COVID-19 Cases")),
+        selectizeInput(
+          inputId = "imp_state",
+          label = "Select one or more States (max 4):",
+          choices = as.character(c("US", states)),
+          selected = c("Maryland"),
+          multiple = TRUE,
+          options = list(maxItems = 4),
+          width = "100%"
+        ),
+        tabBox(width = 12,
+               tabPanel(id = "graph", title = "graph",
+                       uiOutput("body_UI")),
+        
+                tabPanel(id = "map", title = "map", width = "50%",
+                  
+                  div(style = "display: inline-block; vertical-align: center; float:right",
+                         selectizeInput(
+                                inputId = "map",
+                                label = NULL,
+                                choices = c("Confirmed",
+                                            "Deaths",
+                                            "Recovered"),
+                                selected = "Confirmed",
+                                
+                                width = "100%"
+                                
+                              )),
+                  div(style = "display: inline-block; vertical-align: center; margin: auto", h4("Select Variable: ")),
 
-
-          box(
-            h2("Total Number of Specimens Tested for COVID-19 in U.S."),
-            h4(
-              "Data of Confirmed, Deaths and Recovered from",
-              a(href = "https://github.com/CSSEGISandData/COVID-19", "JHU."),
-              br(),
-              "Data of Specimens Tested from",
-              a(href = "https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/testing-in-us.html", "CDC."),
-              hr(style = " margin-top: 1.1em; margin-bottom: 1.2em;")
-            ),
-
-
-            plotlyOutput("tested", height = "400px")
+            leafletOutput("leaf")))),
+      
+     column(6, 
+      box(width = 12,
+        h2("Total Number of Specimens Tested for COVID-19 in U.S."),
+        h4(
+          "Data of Confirmed, Deaths and Recovered from",
+          a(href = "https://github.com/CSSEGISandData/COVID-19", "JHU."),
+          br(),
+          "Data of Specimens Tested from",
+          a(href = "https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/testing-in-us.html", "CDC."),
+          hr(style = " margin-top: 1.1em; margin-bottom: 1.2em;")
+        ),
+        
+        
+        plotlyOutput("tested", height = "400px")
       )
     )
-  )
-)
+  ))))
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-
+  
   # # Get data -----
-
-  withProgress(
-    message = "retrieving the data...",
-    {
-      dat_US <- get_cases()
-      tests <- get_tests()
-    }
-  )
-
-
+  
+  
+  dat_US <- get_cases()
+  tests <- get_tests()
+  
+  
+  
   dat_f <- reactiveVal(NULL)
   dat_m <- reactiveVal(NULL)
-
+  
   # filter data  ------------
   dat_f <- reactive({
     x <- input$imp_state
     dat_US %>%
       filter(state %in% x)
   })
-
+  
   dat_m <- reactive({
     x <- input$map
     dat_f() %>%
       filter(condition ==  x)
   })
-
+  
   # license --------
   observeEvent(
     ignoreNULL = TRUE,
@@ -112,50 +128,56 @@ server <- function(input, output, session) {
       )
     }
   )
-
+  
   # Last update -----
   output$updated <- renderText({
     m_date <- max(dat_f()$date)
-
+    
     x <- paste0("Last Update: ", m_date)
     x
   })
-
-
+  
+  
   # render test plot -----
   output$tested <- renderPlotly({
     p <- plot_tested(tests, log_scale = input$log_scale)
   })
-
-
+  
+  
   # show_p <- reactiveVal(value = FALSE)
   # output$show_p <- reactive({show_p()})
   # outputOptions(output, "show_p", suspendWhenHidden = FALSE)
-
-
+  
+  
   # Render plot_ cases -----
   output$cases <- renderPlotly({
     plot_cases(dat = dat_f(), log_scale = input$log_scale)
   })
-
+  
   output$body_UI <- renderUI({
     n_cond <- length(unique(dat_f()$condition))
-
+    
     if (n_cond <= 1) {
       p <- plotlyOutput("cases", height = "400px")
     }
-
+    
     if (n_cond == 2) {
       p <- plotlyOutput("cases", height = "800px")
     }
-
+    
     if (n_cond == 3) {
       p <- plotlyOutput("cases", height = "1200px")
     }
     p
   })
   
+  # Render Map -------
+  output$leaf <- renderLeaflet({
+    map_leaf(dat = dat_m())
+  })
+  
 
+  
 }
 
 # Run the application
